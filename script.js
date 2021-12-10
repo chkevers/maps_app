5d7d0215d24dc5be6172b205b27c57d581cb4b6e
Christophe Kevers <christophe.kevers@luminus.be>
2021-12-10 17:32:08 +0100

    Adding map

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-// alert('Initial commit')
-let map = L.map('map').setView([51.505, -0.09], 13);
+navigator.geolocation.getCurrentPosition(function(pos) {
+    console.log(pos);
+    const {latitude} = pos.coords;
+    const {longitude} = pos.coords;
+
+    console.log(latitude, longitude);
+},
+function() {
+    alert(BipBop)
+}
+);
+
+
+const map = L.map('map').setView([50.8573188, 4.3562634], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([50.8573188, 4.3562634]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
